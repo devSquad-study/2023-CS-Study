0ed2f91d13247ea96c2850bbbcd3108eece7a6e1
sanghyuk2 <givemesomefoodplz@gmail.com>
2024-02-05 21:59:16 +0900

    feat: [Design] 울기 전략 알고리즘

---
+++ Design/1. 전략 패턴/상혁/duck/strategies/quack/Quack.java
@@ -0,0 +1,8 @@
+package strategies.quack;
+
+public class Quack implements QuackBehavior {
+    @Override
+    public void quack() {
+        System.out.println("꽥꽥!!");
+    }
+}
---
+++ Design/1. 전략 패턴/상혁/duck/strategies/quack/QuackBehavior.java
@@ -0,0 +1,5 @@
+package strategies.quack;
+
+public interface QuackBehavior {
+    public void quack();
+}
