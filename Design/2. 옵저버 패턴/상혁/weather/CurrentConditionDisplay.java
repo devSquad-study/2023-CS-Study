0f3f356c58b2c42be6f35e27262f5a3571ff3105
sanghyuk2 <givemesomefoodplz@gmail.com>
2024-02-06 23:05:50 +0900

    feat: [Design] 날씨 옵저버 클래스

---
+++ Design/2. 옵저버 패턴/상혁/weather/CurrentConditionDisplay.java
@@ -0,0 +1,24 @@
+package observer.weather;
+
+public class CurrentConditionDisplay implements Observer, DisplayElement {
+    private float temperature;
+    private float humidity;
+    private WeatherData weatherData;
+
+    public CurrentConditionDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("현재 상태: 온도 " + temperature + "F, 습도 " + humidity + "%");
+    }
+
+    @Override
+    public void update() {
+        this.temperature = weatherData.getTemperature();
+        this.humidity = weatherData.getHumidity();
+        display();
+    }
+}
