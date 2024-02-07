e768ff56fd2b7940a277da5e25cdaa46e205800b
sanghyuk2 <givemesomefoodplz@gmail.com>
2024-02-07 17:33:25 +0900

    feat: [Design] 피자 팩토리 별 구상 클래스

---
+++ Design/4. 팩토리 패턴/상혁/pizza/PepperoniPizza.java
@@ -0,0 +1,23 @@
+package factory.pizza;
+
+public class PepperoniPizza extends Pizza {
+    @Override
+    public void prepare() {
+
+    }
+
+    @Override
+    public void bake() {
+
+    }
+
+    @Override
+    public void cut() {
+
+    }
+
+    @Override
+    public void box() {
+
+    }
+}
