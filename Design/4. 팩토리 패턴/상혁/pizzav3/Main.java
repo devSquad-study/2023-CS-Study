8055d69a58b18687dae8d09cc4983b047f3dbecb
sanghyuk2 <givemesomefoodplz@gmail.com>
2024-02-07 17:35:44 +0900

    feat: [Design] 피자 주문 시뮬레이터 클래스

---
+++ Design/4. 팩토리 패턴/상혁/pizzav3/Main.java
@@ -0,0 +1,14 @@
+package factory.pizzav3;
+
+public class Main {
+    public static void main(String[] args) {
+        PizzaStore nyPizzaStore = new NYStylePizzaStore();
+        PizzaStore chicagoStore = new ChicagoStylePizzaStore();
+
+        Pizza pizza = nyPizzaStore.orderPizza("cheese");
+        System.out.println("에단이 주문한 " + pizza.getName() + "\n");
+
+        pizza = chicagoStore.orderPizza("cheese");
+        System.out.println("조엘이 주문한 " + pizza.getName() + "\n");
+    }
+}
