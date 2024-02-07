bbb65c059a17a2bfa85beaa19bc9539280b673cc
sanghyuk2 <givemesomefoodplz@gmail.com>
2024-02-07 17:36:30 +0900

    feat: [Design] 피자 제품군 생성 추상 인터페이스

---
+++ Design/4. 팩토리 패턴/상혁/pizzav3/PizzaIngredientFactory.java
@@ -0,0 +1,10 @@
+package factory.pizzav3;
+
+public interface PizzaIngredientFactory {
+    public Dough createDough();
+    public Sauce createSauce();
+    public Cheese createCheese();
+    public Veggies[] createVeggies();
+    public Pepperoni createPepperoni();
+    public Clams createClam();
+}
