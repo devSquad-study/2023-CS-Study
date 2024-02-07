edee64ef119d4bb77d2a21641fbb414893e27595
sanghyuk2 <givemesomefoodplz@gmail.com>
2024-02-07 17:32:51 +0900

    feat: [Design] 피자 가게 클라이언트 클래스

---
+++ Design/4. 팩토리 패턴/상혁/pizza/PizzaStore.java
@@ -0,0 +1,21 @@
+package factory.pizza;
+
+public class PizzaStore {
+    SimplePizzaFactory factory;
+
+    public PizzaStore(SimplePizzaFactory factory) {
+        this.factory = factory;
+    }
+
+    public Pizza orderPizza(String type) {
+        Pizza pizza;
+        pizza = factory.createPizza(type);
+
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+
+        return pizza;
+    }
+}
