2c935fc64f514c1370d952066baf3b9a6942b529
sanghyuk2 <givemesomefoodplz@gmail.com>
2024-02-07 17:36:47 +0900

    feat: [Design] 피자 제품군 생성 구현 클래스

---
+++ Design/4. 팩토리 패턴/상혁/pizzav3/ChicagoIngredientFactory.java
@@ -0,0 +1,34 @@
+package factory.pizzav3;
+
+public class ChicagoIngredientFactory implements PizzaIngredientFactory {
+    @Override
+    public Dough createDough() {
+        return new ThickCrustDough();
+    }
+
+    @Override
+    public Sauce createSauce() {
+        return new PlumTomatoSauce();
+    }
+
+    @Override
+    public Cheese createCheese() {
+        return new MozzarellaCheese();
+    }
+
+    @Override
+    public Veggies[] createVeggies() {
+        Veggies veggies[] = {new Spinach(), new EggPlant(), new BlackOlives()};
+        return veggies;
+    }
+
+    @Override
+    public Pepperoni createPepperoni() {
+        return new SlicedPepperoni();
+    }
+
+    @Override
+    public Clams createClam() {
+        return new FrozenClams();
+    }
+}
